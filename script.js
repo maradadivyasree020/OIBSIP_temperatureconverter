2d9b30eebeb87ed07c44c3f203bd9e3877e32c5d
Marada Divya Sree <maradadivyasree@gmail.com>
2023-06-03 11:57:29 +0530

    commit

---
+++ script.js
@@ -0,0 +1,26 @@
+function far(){
+    let b=document.querySelector('input').value;
+
+    let ans=(b*1.8)+32;
+    console.log(ans);
+
+    let c=document.querySelector('#cel');
+    let f=document.querySelector('#far');  
+
+    c.innerHTML=b;
+    f.innerHTML=ans;
+} 
+
+function car(){
+    let b=document.querySelector('input').value; 
+
+    let ans=((b*1)-32)*0.56;
+    console.log(ans);
+
+    let c=document.querySelector('#cel');
+    let f=document.querySelector('#far');
+
+    c.innerHTML=ans;
+    f.innerHTML=b;
+}
+
